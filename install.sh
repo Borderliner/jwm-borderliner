8b272c8cb5a40a1bcb0debfa5fb71709e73baf41
Psyringe <psyringe@protonmail.com>
2016-01-02 22:07:05 +0330

    Fixed script. It now fully works.

--- install.sh
+++ install.sh
@@ -1,20 +1,27 @@
-echo "Welcome! Make sure you run this file as admin.\n"
+echo "Welcome! DO NOT RUN THIS FILE AS ADMIN."
 git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
 if [ $GIT_IS_AVAILABLE -eq 0 ]; then
-    echo "Installing xdgmenumaker...\n"
+    echo "Installing xdgmenumaker..."
 else
-    echo "Git is not installed. Trying to install git...\n"
-    pacman -S --noconfirm git
+    echo "Git is not installed. Trying to install git..."
+    sudo pacman -S --noconfirm git
 fi
 #Save current dir
-current_dir = $(pwd)
+current_dir=$(pwd)
 
-mkdir -p /tmp/.psyringe
-cd /tmp/.psyringe
+mkdir -p ~/.psytmp/
+cd ~/.psytmp/
 git clone https://github.com/gapan/xdgmenumaker.git
+cd xdgmenumaker
 make
-make install
-echo "Installing necessary xorg components...\n"
-pacman -S --needed --noconfirm xorg-xprop xorg-xwininfo mousepad xlockmore xorg-xclock xfce4-terminal thunar network-manager-applet pa-applet
-cp $current_dir/Config ~/
+sudo make install
+echo "Installing necessary xorg and xfce components..."
+sudo pacman -S --needed --noconfirm xorg-xprop xorg-xwininfo mousepad xlockmore xorg-xclock xfce4-terminal thunar network-manager-applet pa-applet feh
+echo "Copying config files..."
+cd "$current_dir"/Config/
+cp -a . ~/
+echo "Removing temp folder..."
+rm -rf ~/.psytmp/
+echo "Done! Restarting jwm..."
+jwm -restart
